9ff9f30ce6877966d7fa1a6815bed95da373867f
horizon.sh <67441217+matthew956@users.noreply.github.com>
2022-06-04 22:51:57 -0300

    Add files via upload

---
+++ dir.py
@@ -0,0 +1,45 @@
+import requests, os
+
+class bcolors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKCYAN = '\033[96m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+
+dir_list = open("list.txt", "r+")
+
+def clear():
+	os.system('cls|clear')
+
+def msgload():
+	print("""
+█▀▄ █ █▀█   █▀▀ █ █▄░█ █▀▄ █▀▀ █▀█
+█▄▀ █ █▀▄   █▀░ █ █░▀█ █▄▀ ██▄ █▀▄""")
+	print("Feito por horizon.sh")
+	inputs()
+
+def inputs():
+	website = input(f"Website:{bcolors.GREEN} ")
+	loops(website)
+
+
+def loops(website):
+	for lista in  dir_list.readlines():
+		lista = website + lista.rstrip('\n')
+		r = requests.get(lista)
+		if r.status_code != 404:
+			print(f"{bcolors.OKGREEN} [+] {lista} {bcolors.ENDC}")
+
+try:
+	msgload()
+except (KeyboardInterrupt):
+	print(f"{bcolors.WARNING} \n Voce me fechou :( {bcolors.ENDC}")
+except (Exception) as e:
+	print(f"{bcolors.FAIL}\n Erro {bcolors.ENDC} \n\n {e}")
+
+
